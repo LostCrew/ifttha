c7e3a30e0f15b9ba552ab913fe75bee96ee780d2
Emanuele <emanuele.marchi@ecosia.org>
2020-12-25 16:37:57 +0100

    Remove watch check

--- index.js
+++ index.js
@@ -1,16 +1,11 @@
 import * as api from './api'
 
-// const isWatchPage = () => {
-//   return location.pathname.startsWith('/watch/')
-// }
-
 const turnOnScene = async name => {
   const response = await api.postService('scene', 'turn_on', { entity_id: `scene.${name}` })
   console.log(response)
 }
 
 console.log('IFTTHA running!')
-// if (isWatchPage()) {
 document.addEventListener('fullscreenchange', async () => {
   try {
     const scene = document.fullscreenElement ? 'living_room_night' : 'living_room_dusk'
@@ -19,4 +14,3 @@ document.addEventListener('fullscreenchange', async () => {
     console.log(err)
   }
 })
-// }
