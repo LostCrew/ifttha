65b9fd7826f720868d25395e89210bc0faf9340c
Emanuele <emanuele.marchi@ecosia.org>
2021-01-13 01:05:43 +0100

    Add custom types

---
+++ src/types.ts
@@ -0,0 +1,38 @@
+export type AlpineDispatch = (event: string, data: Record<string, unknown>) => unknown
+export type Init = () => () => Promise<void>
+export type RuleID = string
+export type ConditionID = 'fullscreen'
+export type ConditionData = Record<string, string>
+export type Condition = {
+  id: ConditionID
+  data?: ConditionData
+}
+export type Conditions = Condition[]
+export type ActionID = 'scene-turn-on'
+export type ActionEntityID = string
+export type ActionData = Record<string, ActionEntityID | string>
+export type Action = {
+  id: ActionID
+  data?: ActionData
+}
+
+export type Rule = {
+  id: RuleID
+  name: string
+  enabled: boolean
+  conditions: Conditions
+  actionOn: Action
+  actionOff: Action
+}
+
+export type Rules = Rule[]
+
+export type Settings = {
+  apiBaseUrl: string
+  apiToken: string
+}
+
+export type HAData = Record<string, string>
+export type HAResponse = Promise<unknown>
+export type HADomain = string
+export type HAService = string
